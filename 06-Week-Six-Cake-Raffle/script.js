c573ad637744a7b07a167981f91424bc00452785
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-23 20:56:44 +0430

    script js

--- 06-Week-Six-Cake-Raffle/script.js
+++ 06-Week-Six-Cake-Raffle/script.js
@@ -5,3 +5,16 @@ const participantsList = document.getElementById('participants-list');
 const drawButton = document.getElementById('draw-button');
 const resetButton = document.getElementById('reset-button');
 const winnerDisplay = document.getElementById('winner-display');
+
+const participants = [];
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = nameInput.value;
+  participants.push(name);
+
+  participantsList.innerHTML += `<p>${name}</p>`;
+
+  nameInput.value = '';
+});
