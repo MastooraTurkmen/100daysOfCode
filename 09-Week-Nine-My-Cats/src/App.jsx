4c012a319c3885ae93cba0f787cdbb02201c149d
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-22 21:50:28 +0430

    set up app js

--- 09-Week-Nine-My-Cats/src/App.jsx
+++ 09-Week-Nine-My-Cats/src/App.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import Contact from "./Contact";
 import MrWhiskerson from "./images/mr-whiskerson.png";
 import Fluffykins from "./images/fluffykins.png";
+import Felix from "./images/felix.png";
