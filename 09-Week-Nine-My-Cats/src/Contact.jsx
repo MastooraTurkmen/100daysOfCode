6ca47ffc8ee407154e22d301d7a3faa15d3ef161
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-23 21:54:26 +0430

    set up app js

--- 09-Week-Nine-My-Cats/src/Contact.jsx
+++ 09-Week-Nine-My-Cats/src/Contact.jsx
@@ -5,7 +5,9 @@ import PhoneIcone from "./images/phone-icon.png";
 export default function Contact(props) {
   return (
     <div className="contact-card">
-      
+      <img src={props.img} />
+      <h3>{props.name}</h3>
+     
     </div>
   );
 }
