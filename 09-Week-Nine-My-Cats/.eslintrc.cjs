c5cb3ad168faf7f893ed2b6fa80e6d7f7c1c7e3c
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-25 22:13:16 +0430

    set up

--- 09-Week-Nine-My-Cats/.eslintrc.cjs
+++ 09-Week-Nine-My-Cats/.eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+    ],
     
+    },
 }
