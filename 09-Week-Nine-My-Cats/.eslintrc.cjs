79cd5c6166adacb4c1907ee8d2e5dbd5498c45b6
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-25 22:13:37 +0430

    set up

--- 09-Week-Nine-My-Cats/.eslintrc.cjs
+++ 09-Week-Nine-My-Cats/.eslintrc.cjs
@@ -8,6 +8,9 @@ module.exports = {
         'plugin:react-hooks/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
     
     },
 }
